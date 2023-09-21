86bd21cc5c86b0e603c5e9593f7157d94ee796b8
goderxxa <35173779+goderxxa@users.noreply.github.com>
2023-09-21 16:49:20 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    const int MY_HOTKEY_DOWN = 1;
+    const int MY_HOTKEY_UP = 2;
+    const int MY_MOD_CONTROL = MOD_CONTROL | MOD_NOREPEAT;
+
+    // Регистрируем горячие клавиши Ctrl+Down и Ctrl+Up
+    if (!RegisterHotKey(NULL, MY_HOTKEY_DOWN, MY_MOD_CONTROL, VK_DOWN)) {
+        std::cerr << "Не удалось зарегистрировать горячую клавишу Ctrl+Down!" << std::endl;
+        return 1;
+    }
+    if (!RegisterHotKey(NULL, MY_HOTKEY_UP, MY_MOD_CONTROL, VK_UP)) {
+        std::cerr << "Не удалось зарегистрировать горячую клавишу Ctrl+Up!" << std::endl;
+        return 1;
+    }
+
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0) != 0) {
+        if (msg.message == WM_HOTKEY) {
+            if (msg.wParam == MY_HOTKEY_DOWN) {
+                keybd_event(VK_VOLUME_DOWN, 0, KEYEVENTF_KEYUP, 0); // Сначала отпустим клавишу (если она уже нажата)
+                keybd_event(VK_VOLUME_DOWN, 0, 0, 0); // Затем нажмем клавишу
+                keybd_event(VK_VOLUME_DOWN, 0, KEYEVENTF_KEYUP, 0); // И отпустим снова
+            }
+            else if (msg.wParam == MY_HOTKEY_UP) {
+                keybd_event(VK_VOLUME_UP, 0, KEYEVENTF_KEYUP, 0); // Сначала отпустим клавишу (если она уже нажата)
+                keybd_event(VK_VOLUME_UP, 0, 0, 0); // Затем нажмем клавишу
+                keybd_event(VK_VOLUME_UP, 0, KEYEVENTF_KEYUP, 0); // И отпустим снова
+            }
+        }
+    }
+
+    // Отменяем регистрацию горячих клавиш перед завершением программы
+    UnregisterHotKey(NULL, MY_HOTKEY_DOWN);
+    UnregisterHotKey(NULL, MY_HOTKEY_UP);
+
+    return 0;
+}
